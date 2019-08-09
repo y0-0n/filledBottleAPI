e97d722e74c3550eb2939e881807f965190c1aeb
L5vD5 <l5vd5@korea.ac.kr>
2019-08-09 18:46:04 +0900

    제품 추가시 재고 테이블 초기화

--- src/routes/product.js
+++ src/routes/product.js
@@ -37,13 +37,33 @@ router.get('/product/:id', function(req, res){
 });
 
 router.post('/product', (req, res) => {
+
   connection.query("INSERT INTO `product` (`name`, `image`, `group`, `barcode`, `price_receiving`, `price_shipping`, `category`, `is_set`, `process`) VALUES ('"+req.body.name+"', '2', '3', '4', '5', '6', '7', '8', '9');", function(err, rows) {
     if(err) throw err;
 
     console.log('POST /product : ' + rows);
-    res.header("Access-Control-Allow-Origin", "*");
 
-    res.send(rows);
+    const product_id = rows.insertId;
+    
+    connection.query("SELECT * FROM plant", function(err_, rows_) {
+      let plant_ids = [];
+      if(err) throw err;
+      rows_.map((e, i) => {
+        plant_ids.push(e.id);
+      });
+
+      plant_ids.map((e, i) => {
+        connection.query(`INSERT INTO stock (\`product_id\`, \`plant_id\`, \`quantity\`) VALUES ('${product_id}', '${e}', '${0}')`, function(err_, rows_) {
+          if(err_) throw err_;
+          console.log('stock '+rows_)
+        });
+      })
+      
+  
+      res.header("Access-Control-Allow-Origin", "*");
+  
+      res.send(rows);  
+    })
   });
 });
 
@@ -55,7 +75,7 @@ router.options('/product', (req, res, next) => {
 });
 
 router.delete('/product', function(req, res){
-  connection.query("DELETE FROM product WHERE  `id`="+req.body.id+";", function(err, rows) {
+  connection.query(`DELETE FROM product WHERE \`id\`=${req.body.id};`, function(err, rows) {
     if(err) throw err;
 
     console.log('DELETE /product : ' + rows);
