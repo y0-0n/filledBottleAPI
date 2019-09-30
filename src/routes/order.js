d730707ca2e7286a7c663a9b18e8f5b0c447cded
L5vD5 <l5vd5@korea.ac.kr>
2019-09-30 13:16:58 +0900

    상태별 주문 검색 API 변경

--- src/routes/order.js
+++ src/routes/order.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const connection = require('../../config/dbConnection').connection;
 
 router.get('/order', function(req, res){
-  connection.query('SELECT A.id, A.state, A.date, A.price, A.received, B.name from `order` AS A JOIN `customer` AS B WHERE A.customer_id = B.id ORDER BY A.date DESC', function(err, rows) {
+  connection.query('SELECT A.id, A.state, A.date, A.price, A.received, B.name, A.orderDate from `order` AS A JOIN `customer` AS B ON A.customer_id = B.id ORDER BY A.orderDate DESC', function(err, rows) {
     if(err) throw err;
 
     console.log('GET /order : ' + rows);
@@ -15,11 +15,11 @@ router.get('/order', function(req, res){
 });
 
 router.get('/order/:state', function(req, res){
-  let {state} = req.params; // 검색어로 검색
-  connection.query(`SELECT * from \`order\` WHERE state = '${state}'`, function(err, rows) {
+  let {state} = req.params; // 상태로 검색
+  connection.query(`SELECT A.id, A.state, A.date, A.price, A.received, B.name, A.orderDate from \`order\` AS A JOIN \`customer\` AS B ON A.customer_id = B.id WHERE A.state = '${state}' ORDER BY A.orderDate DESC`, function(err, rows) {
     if(err) throw err;
 
-    console.log('GET /order : ' + rows);
+    console.log('GET /order/:state : ' + rows);
     res.header("Access-Control-Allow-Origin", "*");
     res.send(rows);
   });
@@ -47,13 +47,13 @@ router.get('/order_summary', function(req, res) {
 
 router.post('/order', (req, res) => {
   let price = 0; //총액
-  let {sCustomer, sProduct, date, cellphone, telephone, address, comment} = req.body;
+  let {sCustomer, sProduct, date, cellphone, telephone, address, comment, orderDate} = req.body;
   console.warn(req.body)
   sProduct.map((e, i) => {
     price += e.quantity * e.price; // 수량 * 출고 가격
   })
 
-  connection.query(`INSERT INTO \`order\` (\`customer_id\`, \`date\`, \`price\`, \`telephone\`, \`cellphone\`, \`address\`, \`comment\`) VALUES ('${sCustomer}', '${date}', '${price}', '${telephone}', '${cellphone}', '${address}', '${comment}')`, function(err, rows) {
+  connection.query(`INSERT INTO \`order\` (\`customer_id\`, \`date\`, \`price\`, \`telephone\`, \`cellphone\`, \`address\`, \`comment\`, \`orderDate\`) VALUES ('${sCustomer}', '${date}', '${price}', '${telephone}', '${cellphone}', '${address}', '${comment}', '${orderDate}')`, function(err, rows) {
     if(err) throw err;
     console.log('POST /order : ' + rows);
 
@@ -92,13 +92,13 @@ router.delete('/customer', function(req, res){
 router.get('/orderDetail/:id', function(req, res){
   let {id} = req.params; // id로 검색
 
-  connection.query(`SELECT date, name, o.address as address, o.telephone as telephone, o.cellphone as cellphone, comment from \`order\` as o JOIN \`customer\` as c ON o.customer_id = c.id WHERE o.id=${id}`, function(err, rows) {
+  connection.query(`SELECT date, name, o.address as address, o.telephone as telephone, o.cellphone as cellphone, comment, state from \`order\` as o JOIN \`customer\` as c ON o.customer_id = c.id WHERE o.id=${id}`, function(err, rows) {
     if(err) throw err;
 
     connection.query(`SELECT * from \`order\` as o JOIN \`order_product\` as op ON o.id = op.order_id JOIN product as p ON op.product_id = p.id WHERE o.id=${id}`, function(err, rows2) {
       if(err) throw err;
 
-      console.log('GET /orderDetail' + id + ' : ' + rows);
+      console.log('GET /orderDetail/' + id + ' : ' + rows);
       res.header("Access-Control-Allow-Origin", "*");
       res.send({orderInfo: rows, productInfo: rows2});
     });
@@ -111,6 +111,25 @@ router.get('/orderDetail/:id', function(req, res){
     res.header("Access-Control-Allow-Origin", "*");
     res.send(rows);
   });*/
+});
+
+router.put('/order/changeState/:id/:state', function(req, res) {
+  let {id, state} = req.params;
+
+  connection.query(`UPDATE \`order\` SET \`state\`='${state}' WHERE \`id\`=${id}`, function(err, rows) {
+    if(err) throw err;
+
+    console.log(`UPDATE /order/changeState/${id}/${state}` + rows);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send(rows);
+  });
+});
+
+router.options('/order/changeState/:id/:state', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
 });
 
 module.exports = router;
