95ece5c4233a17ad571cddb5d81ab33cde7189ad
L5vD5 <l5vd5@korea.ac.kr>
2020-03-04 16:53:16 +0900

    SQL 다중 쿼리 허용

--- config/database.js
+++ config/database.js
@@ -6,13 +6,15 @@ module.exports = {
     port     : 3306,
     database : 'en',
     connectionLimit: 20,
-    waitForConnections: false
+		waitForConnections: false,
+		multipleStatements: true,
   },
   conn: {
     host     : '211.62.225.216',
     user     : 'l5vd5',
     password : 'dbsqud23',
     port     : 3306,
-    database : 'en'
+		database : 'en',
+		multipleStatements: true,
   }
 };
