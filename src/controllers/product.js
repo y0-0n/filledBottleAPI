bd1b00a1bc3ac5dc3d9f604e0cf8f4432e7ee6de
L5vD5 <l5vd5@korea.ac.kr>
2020-04-01 14:03:41 +0900

    쌀만 품목 두개씩 주던것 수정

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -55,11 +55,10 @@ router.post('/total/unset/', checkAuthed, function(req, res) {
 });
 
 router.post('/list', checkAuthed, function(req, res){
-  let {page, name, family, category} = req.body;
+	let {page, name, family, category} = req.body;
   connection.query(`SELECT A.id as id, A.\`name\` as name, A.grade, A.price_shipping, weight, file_name, F.\`name\` as familyName
 		FROM product as A JOIN users as B ON A.user_id = B.id
-		LEFT JOIN productFamily_user as FU ON A.family = FU.family_id
-		LEFT JOIN productFamily as F ON F.id = FU.family_id
+		LEFT JOIN productFamily as F ON F.id = A.family
 		WHERE \`set\`=1
 		AND B.id = '${req.user.id}'
 		${family !== 0 ? `AND A.family = '${family}'` : ``}
