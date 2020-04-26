c88d8fd5ec6f5928593c6e684e8e80df9f2e7844
L5vD5 <l5vd5@korea.ac.kr>
2020-04-27 01:31:32 +0900

    sequelize 도입

--- src/controllers/api/admin/users/users.js
+++ src/controllers/api/admin/users/users.js
@@ -1,8 +1,14 @@
 const Users = require('../../../../models/Users');
-
+const models = require('../../../../../models');
 exports.getListAdmin = (req, res) => {
   Users.getListAdmin(req.user.id, req.body, (err, rows) => {
     if(err) throw err;
+    models.users.findAll()
+    .then(result => {
+      //sequelize 도입 성공 예시
+      console.warn(JSON.stringify(result))
+      //res.status(200).send(result)
+    })
     res.status(200).send(rows);
   })
 }
