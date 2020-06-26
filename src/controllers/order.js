f1755714ff145b72bfa8ebd470b86b53e0ea3bce
L5vD5 <l5vd5@korea.ac.kr>
2020-06-26 16:50:23 +0900

    주문 페이지에서 주소 분리

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -173,7 +173,7 @@ router.post('/', (req, res) => {
 router.get('/detail/:id', checkAuthed, function(req, res){
   let {id} = req.params; // id로 검색
 
-  connection.query(`SELECT o.id as id, date, name, o.address as address, o.telephone as telephone, o.cellphone as cellphone, comment, state, o.user_id from \`order\` as o JOIN \`customer\` as c ON o.customer_id = c.id WHERE o.id=${id}`, function(err, rows) {
+  connection.query(`SELECT o.id as id, date, name, o.address as address, o.address_detail as addressDetail, o.postcode as postcode, o.telephone as telephone, o.cellphone as cellphone, comment, state, o.user_id from \`order\` as o JOIN \`customer\` as c ON o.customer_id = c.id WHERE o.id=${id}`, function(err, rows) {
     if(err) throw err;
     if(rows.length === 0 || rows[0]['user_id'] !== req.user.id) {
       res.status(400).send({message: '400 Error'});
