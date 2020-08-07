df78c241f933ac222da3543d4262d8033e681794
yhc002 <yhc002@gmail.com>
2020-08-06 20:10:12 -0700

    product post, put: get shippingEndDate, gap, weight from data

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -116,7 +116,7 @@ router.get('/:id', checkAuthed, function(req, res) {
 });
 
 router.post('/', checkAuthed, upload.fields([{name: 'file'}, {name: 'file_detail'}]), (req, res) => {
-  let {name, price, grade, weight, productFamily, discount_price, state, vat, shippingDate, additional} = req.body;
+  let {name, price, grade, weight, productFamily, discount_price, state, vat, shippingDate, shippingEndDate, additional, gap} = req.body;
 	let fileName = 'noimage.jfif';
 	if(req.files.file)
 		 fileName = 'product/'+req.files.file[0].filename; // 대표 이미지
@@ -128,10 +128,9 @@ router.post('/', checkAuthed, upload.fields([{name: 'file'}, {name: 'file_detail
 		})
 		detailFileName = detailFileName.slice(0, -1);
   }
-  weight=0 //영헌: api 변경에 따른 임시 dummy data
-  let gap=null //영헌: api 변경에 따른 임시 dummy data
-  connection.query(`INSERT INTO \`product\` (\`name\`, \`grade\`, \`barcode\`, \`price_receiving\`, \`price_shipping\`, \`discount_price\`, \`weight\`, \`safety_stock\`, \`file_name\`, \`detail_file\`, \`user_id\`, \`family\`, \`state\`, \`tax\`, \`shippingDate\`, \`additional\`, \`gap\`)
-                    VALUES ('${name}', '${grade}', '4', '5', '${price}', '${discount_price}', '${weight}', '8', '${fileName}', '${detailFileName}', "${req.user.id}", ${productFamily}, ${state}, ${vat}, '${shippingDate}', '${additional}', '${gap}');`, function(err, rows) {
+  connection.query(`INSERT INTO \`product\` (\`name\`, \`grade\`, \`barcode\`, \`price_receiving\`, \`price_shipping\`, \`discount_price\`, \`weight\`, \`safety_stock\`, \`file_name\`, \`detail_file\`, \`user_id\`, \`family\`, \`state\`, \`tax\`, \`shippingDate\`, \`shippingEndDate\`, \`additional\`, \`gap\`)
+                    VALUES ('${name}', '${grade}', '4', '5', '${price}', '${discount_price}', '${weight}', '8', '${fileName}', '${detailFileName}', "${req.user.id}", ${productFamily}, ${state}, ${vat}, '${shippingDate}', '${shippingEndDate}', '${additional}', '${gap}');`, function(err, rows) {
+
     if(err) throw err;
 
     console.log('POST /product : ', rows);
@@ -174,7 +173,7 @@ router.put('/deactivate', checkAuthed, function(req, res){
 });
 
 router.put('/modify/:id', checkAuthed, upload.fields([{name: 'file'}, {name: 'file_detail'}]), function(req, res) {
-  const { name, price, productFamily, discount_price, state, shippingDate, additional } = req.body;
+  const { name, price, weight, productFamily, discount_price, state, shippingDate, shippingEndDate, additional, gap } = req.body;
   let fileName = 'noimage.jfif';
   // console.warn(req.body)
 	if(req.files.file)
@@ -186,9 +185,7 @@ router.put('/modify/:id', checkAuthed, upload.fields([{name: 'file'}, {name: 'fi
 		})
 		detailFileName = detailFileName.slice(0, -1);
   }
-  let gap=null //영헌: api 변경에 따른 임시 dummy data
-	// console.warn(req.files);
-  connection.query(`UPDATE product SET \`name\`='${name}', \`price_shipping\`='${price}', \`discount_price\`='${discount_price}', \`family\` ='${productFamily}', \`state\` = '${state}', \`file_name\`='${fileName}', \`detail_file\`='${detailFileName}', \`shippingDate\` ='${shippingDate}', \`additional\`='${additional}',  \`gap\`='${gap}' WHERE \`id\`=${req.params.id};`, function(err, rows) {
+  connection.query(`UPDATE product SET \`name\`='${name}', \`weight\`='${weight}', \`price_shipping\`='${price}', \`discount_price\`='${discount_price}', \`family\` ='${productFamily}', \`state\` = '${state}', \`file_name\`='${fileName}', \`detail_file\`='${detailFileName}', \`shippingDate\` ='${shippingDate}', \`shippingEndDate\` ='${shippingEndDate}', \`additional\`='${additional}',  \`gap\`='${gap}' WHERE \`id\`=${req.params.id};`, function(err, rows) {
     if(err) throw err;
 
     console.log('PUT /product/modify/:id : ', rows);
